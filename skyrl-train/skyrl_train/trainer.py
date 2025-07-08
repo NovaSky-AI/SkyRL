4fd61b0aff64a2a3b5c5c9627dc329818d432584
Eric Tang <erictang000@gmail.com>
2025-07-08 06:24:43 +0000

    x

--- skyrl-train/skyrl_train/trainer.py
+++ skyrl-train/skyrl_train/trainer.py
@@ -201,7 +201,7 @@ class RayPPOTrainer:
             self.policy_model, self.inference_engine_client, self.cfg.trainer.placement.colocate_all
         )
         self.eval_weights_manager = InferenceWeightsManager(
-            self.policy_model, self.inference_engine_client, self.cfg.trainer.placement.colocate_all, no_sync=True
+            self.policy_model, self.inference_engine_client, self.cfg.trainer.placement.colocate_all, no_sync=False
         )
 
         # Load checkpoint state if resumption is enabled
