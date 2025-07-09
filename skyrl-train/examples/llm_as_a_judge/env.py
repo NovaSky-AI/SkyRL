d7f774b3138be606d37a0c3b3c283ffe1b81465a
Eric Tang <erictang000@gmail.com>
2025-07-09 21:51:46 +0000

    merge

---
+++ skyrl-train/examples/llm_as_a_judge/env.py
@@ -0,0 +1,66 @@
+from skyrl_gym.envs.base_text_env import BaseTextEnv, BaseTextEnvStepOutput
+from typing import Any
+from typing import Dict
+from omegaconf import DictConfig
+from openai import OpenAI
+
+
+class GSM8kLLMJudgeEnv(BaseTextEnv):
+    """
+    Example implementtion of GSM8k environment with LLM as judge.
+
+    Use LLM as judge to evaluate the math problem solving query similarity with the gold math problem solving query.
+    """
+
+    def __init__(self, env_config: DictConfig, extras: Dict[str, Any] = {}):
+        super().__init__()
+
+        assert "reward_spec" in extras, "reward_spec field is required"
+        assert "ground_truth" in extras["reward_spec"], "ground_truth is required in reward_spec field"
+        self.ground_truth = extras["reward_spec"]["ground_truth"]
+
+        # Set up OpenAI client
+        openai_api_key = env_config.OPENAI_API_KEY
+        self.llm_judge_client = OpenAI(api_key=openai_api_key)
+        self.model = env_config.model
+
+    def _get_reward(self, action: str) -> float:
+        # Use LLM as judge to evaluate the math problem solving query similarity with the gold math problem solving query.
+        prompt = f"""
+            You are a strict math evaluation assistant.
+
+            Compare the following **gold** and **predicted** math solutions. 
+            Determine if the predicted solution follows valid reasoning and reaches the correct final answer, even if the explanation differs in wording.
+
+            Rules:
+            - Only answer "1" if the predicted solution is mathematically correct and leads to the same final answer as the gold solution.
+            - Otherwise, answer "0".
+            - Do not include any explanation or extra text—output only a single character: "1" or "0".
+
+            GOLD SOLUTION:
+            {self.ground_truth}
+
+            PREDICTED SOLUTION:
+            {action}
+
+            Answer:
+        """
+
+        try:
+            response = self.llm_judge_client.chat.completions.create(
+                model=self.model,
+                messages=[{"role": "user", "content": prompt}],
+                max_tokens=1,
+            )
+            reply = response.choices[0].message.content.strip()
+            return 1.0 if reply == "1" else 0.0
+        except Exception as e:
+            print(f"LLM Judge error: {type(e).__name__}: {e}")
+            return 0.0
+
+    def step(self, action: str) -> BaseTextEnvStepOutput:
+        done = True  # always done after one step
+        reward = self._get_reward(action)
+
+        # No observation in gsm8k, and no tool call
+        return BaseTextEnvStepOutput(observations=[], reward=reward, done=done, metadata={})
