d8dae042cdf6be23875a30bc0279d28e9583a575
SumanthRH <sumanthrh99@gmail.com>
2025-07-10 00:07:36 +0000

    x
    Signed-off-by: SumanthRH <sumanthrh99@gmail.com>

--- skyrl-train/skyrl_train/inference_engines/vllm/vllm_engine.py
+++ skyrl-train/skyrl_train/inference_engines/vllm/vllm_engine.py
@@ -16,7 +16,6 @@ from skyrl_train.inference_engines.base import (
     InferenceEngineOutput,
     NamedWeightUpdateRequest,
 )
-from skyrl_train.utils import str_to_torch_dtype
 
 
 def setup_envvars_for_vllm(kwargs, bundle_indices):
@@ -91,7 +90,6 @@ class WorkerWrap:
         """Broadcast weight to all vllm workers from source rank 0 (actor model)"""
         import torch
 
-        dtype = str_to_torch_dtype(dtype)
         assert dtype == self.model_config.dtype, f"mismatch dtype: src {dtype}, dst {self.model_config.dtype}"
         weight = torch.empty(shape, dtype=dtype, device="cuda")
         torch.distributed.broadcast(weight, 0, group=self._model_update_group)
@@ -103,7 +101,6 @@ class WorkerWrap:
     def update_weight_cuda_ipc(self, name: str, dtype: str, shape: List[int], ipc_handles: Dict[str, Any]):
         import torch
 
-        dtype = str_to_torch_dtype(dtype)
         device = torch.cuda.current_device()
         props = torch.cuda.get_device_properties(device)
         physical_gpu_id = str(props.uuid)
