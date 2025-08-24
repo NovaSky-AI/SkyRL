44ce853da7f33c503d2c0fa419ae6d0ad9389a70
Charlie Ruan <charlieruan@berkeley.edu>
2025-08-24 20:03:53 +0000

    [Generator][Env] Add stop str, remove need for postprocessed action for search and txt2sql

--- skyrl-gym/skyrl_gym/envs/search/env.py
+++ skyrl-gym/skyrl_gym/envs/search/env.py
@@ -56,13 +56,11 @@ class SearchEnv(BaseTextEnv):
             return True
         return "<answer>" in action and "</answer>" in action
 
-    def _postprocess_action(self, action: str) -> str:
+    def _validate_action(self, action: str):
         if "</search>" in action:
-            return action.split("</search>")[0] + "</search>"
+            assert action.split("</search>")[1] == "", "</search> detected in the response but it is not the last string generated. Use \"</search>\" and \"</answer>\" as stop strings in the configuration."
         elif "</answer>" in action:
-            return action.split("</answer>")[0] + "</answer>"
-        else:
-            return action
+            assert action.split("</answer>")[1] == "", "</answer> detected in the response but it is not the last string generated. Use \"</search>\" and \"</answer>\" as stop strings in the configuration."
 
     def _execute_tool(self, tool_group_name: str, tool_name: str, tool_input: Any) -> str:
         tool_output = super()._execute_tool(tool_group_name, tool_name, tool_input)
@@ -71,7 +69,7 @@ class SearchEnv(BaseTextEnv):
 
     def step(self, action: str) -> BaseTextEnvStepOutput:
         self.turns += 1
-        action = self._postprocess_action(action)
+        self._validate_action(action)
         self.chat_history.append({"role": "assistant", "content": action})
 
         error = None
@@ -80,7 +78,7 @@ class SearchEnv(BaseTextEnv):
 
         if done:
             return BaseTextEnvStepOutput(
-                observations=[], reward=reward, done=done, metadata={}, postprocessed_action=action
+                observations=[], reward=reward, done=done, metadata={}
             )
 
         try:
@@ -114,5 +112,4 @@ class SearchEnv(BaseTextEnv):
             reward=reward,
             done=done,
             metadata=info,
-            postprocessed_action=action,
         )
--- skyrl-gym/skyrl_gym/envs/sql/env.py
+++ skyrl-gym/skyrl_gym/envs/sql/env.py
@@ -89,17 +89,15 @@ class SQLEnv(BaseTextEnv):
             return True
         return "<solution>" in action and "</solution>" in action
 
-    def _postprocess_action(self, action: str) -> str:
+    def _validate_action(self, action: str):
         if "</sql>" in action:
-            return action.split("</sql>")[0] + "</sql>"
+            assert action.split("</sql>")[1] == "", "</sql> detected in the response but it is not the last string generated. Use \"</sql>\" and \"</solution>\" as stop strings in the configuration."
         elif "</solution>" in action:
-            return action.split("</solution>")[0] + "</solution>"
-        else:
-            return action
+            assert action.split("</solution>")[1] == "", "</solution> detected in the response but it is not the last string generated. Use \"</sql>\" and \"</solution>\" as stop strings in the configuration."
 
     def step(self, action: str) -> BaseTextEnvStepOutput:
         self.turns += 1
-        action = self._postprocess_action(action)
+        self._validate_action(action)
         self.chat_history.append({"role": "assistant", "content": action})
 
         error = None
@@ -107,9 +105,7 @@ class SQLEnv(BaseTextEnv):
         reward = self._get_reward(action, done)
 
         if done:
-            return BaseTextEnvStepOutput(
-                observations=[], reward=reward, done=done, metadata={}, postprocessed_action=action
-            )
+            return BaseTextEnvStepOutput(observations=[], reward=reward, done=done, metadata={})
 
         try:
             tool_group_name, tool_name, tool_input = self._parse_action(action)
@@ -140,5 +136,4 @@ class SQLEnv(BaseTextEnv):
             reward=reward,
             done=done,
             metadata=info,
-            postprocessed_action=action,
         )
