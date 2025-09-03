db982e0d778bb5077b420a590126a0ec1892b4ca
SumanthRH <sumanthrh99@gmail.com>
2025-09-03 23:46:07 +0000

    x
    Signed-off-by: SumanthRH <sumanthrh99@gmail.com>

--- skyrl-train/examples/remote_inference_engine/run_vllm_server.sh
+++ skyrl-train/examples/remote_inference_engine/run_vllm_server.sh
@@ -4,13 +4,13 @@ set -x
 
 # NOTE (sumanthrh): Currently, there's an issue with distributed executor backend ray for vllm 0.9.2.
 # For standalone server, we use mp for now. 
-CUDA_VISIBLE_DEVICES=3 uv run --isolated --extra vllm --env-file examples/mini_swe_agent/.env.miniswe -m skyrl_train.inference_engines.vllm.vllm_server \
-    --model Qwen/Qwen3-4B \
-    --tensor-parallel-size 1 \
+CUDA_VISIBLE_DEVICES=4,5,6,7 uv run --isolated --extra vllm -m skyrl_train.inference_engines.vllm.vllm_server \
+    --model Qwen/Qwen2.5-1.5B-Instruct \
+    --tensor-parallel-size 4 \
     --host 127.0.0.1 \
     --port 8001 \
     --seed 42 \
-    --max-model-len 32768 \
+    --max-model-len 4096 \
     --enable-prefix-caching \
     --enable-chunked-prefill \
     --dtype bfloat16 \
@@ -19,5 +19,5 @@ CUDA_VISIBLE_DEVICES=3 uv run --isolated --extra vllm --env-file examples/mini_s
     --max-num_batched_tokens 8192 \
     --max-num-seqs 1024 \
     --trust-remote-code \
-    --distributed-executor-backend ray \
+    --distributed-executor-backend mp \
     --worker-extension-cls skyrl_train.inference_engines.vllm.vllm_engine.WorkerWrap
