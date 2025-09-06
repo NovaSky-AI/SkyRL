b0c8566d7689aca50f70f03b3f42d8719bf13011
Eric Tang <erictang000@gmail.com>
2025-09-06 01:27:14 +0000

    fixed dp convergence maybe?

--- skyrl-train/tests/gpu/test_megatron_worker.py
+++ skyrl-train/tests/gpu/test_megatron_worker.py
@@ -9,7 +9,7 @@ import hydra
 from omegaconf import DictConfig
 import torch
 import asyncio
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM
 
 from tests.gpu.utils import (
     init_worker_with_type,
@@ -54,46 +54,50 @@ def get_test_training_batch() -> TrainingInputBatch:
     Attention masks are 1 for non-padding tokens, 0 for padding tokens
     The rest of the fields are filled with dummy data
     """
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+    import pickle
 
-    sentences = [
-        "<|im_start|>system\nYou are Qwen, created by Alibaba Cloud. You are a helpful assistant.",
-        "<|im_start|>user\nThe selling price of a bicycle that had sold $220 last year was increased by 15",
-        "What is the new price? Let's think step by step and output the final answer after `####`.<|im_end|>\n",
-        "<|im_start|>assistant\nTo find the new price of the bicycle after the increase,",
-    ]
+    with open("/mnt/cluster_storage/gsm8k_batch.pkl", "rb") as f:
+        data = pickle.load(f)[10:74]
+    # tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
-    sequences = [tokenizer.encode(sentence) for sentence in sentences]
-    attention_masks = [[1] * len(seq) for seq in sequences]
-    batch_size = len(sequences)
-    num_actions = 10
+    # sentences = [
+    #     "<|im_start|>system\nYou are Qwen, created by Alibaba Cloud. You are a helpful assistant.",
+    #     "<|im_start|>user\nThe selling price of a bicycle that had sold $220 last year was increased by 15",
+    #     "What is the new price? Let's think step by step and output the final answer after `####`.<|im_end|>\n",
+    #     "<|im_start|>assistant\nTo find the new price of the bicycle after the increase,",
+    # ]
 
-    pad_token_id = tokenizer.pad_token_id
+    # sequences = [tokenizer.encode(sentence) for sentence in sentences]
+    # attention_masks = [[1] * len(seq) for seq in sequences]
+    # batch_size = len(sequences)
+    # num_actions = 10
 
-    # pad to length of longest sequence (25)
-    pad_before_after = [(4, 2), (0, 1), (1, 1), (6, 4)]
-    for i, (pad_before, pad_after) in enumerate(pad_before_after):
-        sequences[i] = [pad_token_id] * pad_before + sequences[i] + [pad_token_id] * pad_after
-        attention_masks[i] = [0] * pad_before + attention_masks[i] + [0] * pad_after
+    # pad_token_id = tokenizer.pad_token_id
 
-    attention_masks = torch.tensor(attention_masks)
-    sequences = torch.tensor(sequences)
+    # # pad to length of longest sequence (25)
+    # pad_before_after = [(4, 2), (0, 1), (1, 1), (6, 4)]
+    # for i, (pad_before, pad_after) in enumerate(pad_before_after):
+    #     sequences[i] = [pad_token_id] * pad_before + sequences[i] + [pad_token_id] * pad_after
+    #     attention_masks[i] = [0] * pad_before + attention_masks[i] + [0] * pad_after
 
-    data = TrainingInputBatch(
-        {
-            "sequences": sequences,
-            "attention_mask": attention_masks,
-            "action_log_probs": torch.tensor([[0.1] * num_actions] * batch_size),
-            "base_action_log_probs": torch.tensor([[0.2] * num_actions] * batch_size),
-            "rollout_logprobs": torch.tensor([[0.11] * num_actions] * batch_size),
-            "values": torch.tensor([[0.1] * num_actions] * batch_size),
-            "returns": torch.tensor([[0.1] * num_actions] * batch_size),
-            "advantages": torch.tensor([[0.5] * num_actions] * batch_size),
-            "loss_mask": torch.tensor([[1] * num_actions] * batch_size),
-            "response_mask": torch.tensor([[1] * num_actions] * batch_size),
-        }
-    )
-    data.metadata = {"response_length": num_actions}
+    # attention_masks = torch.tensor(attention_masks)
+    # sequences = torch.tensor(sequences)
+
+    # data = TrainingInputBatch(
+    #     {
+    #         "sequences": sequences,
+    #         "attention_mask": attention_masks,
+    #         "action_log_probs": torch.tensor([[0.1] * num_actions] * batch_size),
+    #         "base_action_log_probs": torch.tensor([[0.2] * num_actions] * batch_size),
+    #         "rollout_logprobs": torch.tensor([[0.11] * num_actions] * batch_size),
+    #         "values": torch.tensor([[0.1] * num_actions] * batch_size),
+    #         "returns": torch.tensor([[0.1] * num_actions] * batch_size),
+    #         "advantages": torch.tensor([[0.5] * num_actions] * batch_size),
+    #         "loss_mask": torch.tensor([[1] * num_actions] * batch_size),
+    #         "response_mask": torch.tensor([[1] * num_actions] * batch_size),
+    #     }
+    # )
+    # data.metadata = {"response_length": num_actions}
     return data
 
 
@@ -350,6 +354,11 @@ async def test_megatron_dp(cfg, ray_init_fixture, worker_type, tp, pp, gpus_per_
     cfg.generator.n_samples_per_prompt = 1
     cfg.trainer.micro_train_batch_size_per_gpu = 4
 
+    # set torch profiler config
+    cfg.trainer.policy.megatron_config.torch_profiler_config.enable = True
+    cfg.trainer.policy.megatron_config.torch_profiler_config.ranks = [0]
+    cfg.trainer.policy.megatron_config.torch_profiler_config.save_path = "/home/ray/megatron_prof/tp2_pp2/"
+
     actor_group = init_worker_with_type(
         "policy",
         shared_pg=None,
@@ -382,6 +391,9 @@ async def test_megatron_dp(cfg, ray_init_fixture, worker_type, tp, pp, gpus_per_
     # check the grad norm for the same thing but with pp=1, tp=1, dp=4
     cfg.trainer.policy.megatron_config.tensor_model_parallel_size = 1
     cfg.trainer.policy.megatron_config.pipeline_model_parallel_size = 1
+
+    # set torch profiler config
+    cfg.trainer.policy.megatron_config.torch_profiler_config.save_path = "/home/ray/megatron_prof/dp4/"
 
     # set batch sizes correctly
     cfg.trainer.train_batch_size = 64
