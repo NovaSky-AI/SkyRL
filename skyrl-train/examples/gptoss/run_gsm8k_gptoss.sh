03f3427940224de8697bac11f832c062e32eca39
SumanthRH <sumanthrh99@gmail.com>
2025-10-04 23:09:52 +0000

    x
    Signed-off-by: SumanthRH <sumanthrh99@gmail.com>

--- skyrl-train/examples/gptoss/run_gsm8k_gptoss.sh
+++ skyrl-train/examples/gptoss/run_gsm8k_gptoss.sh
@@ -3,7 +3,7 @@ set -x
 # Colocated GRPO training+generation for GPT-OSS-20B on GSM8K.
 # NOTE (sumanthrh): Currently, gpt-oss requires flash attention to be disabled since attention sinks are not supported: https://github.com/Dao-AILab/flash-attention/issues/1797
 # We thus disable flash attention as well as sample packing
-# We only support BF16 training for single-turn tasks at the moment, and are actively working on multi-turn support.
+# We only support GPT-OSS training in BF16 precision and single-turn tasks at the moment, and are actively working on multi-turn support.
 
 # uv run examples/gsm8k/gsm8k_dataset.py --output_dir $HOME/data/gsm8k
 # export WANDB_API_KEY=<your_key_here>
