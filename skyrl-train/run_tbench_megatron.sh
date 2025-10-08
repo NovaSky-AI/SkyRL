5ffe3f35236755b508b90e925c1f0bedd540e3fe
Harryllh <liheng@berkeley.edu>
2025-10-08 21:57:33 +0000

    working megatron

--- skyrl-train/run_tbench_megatron.sh
+++ skyrl-train/run_tbench_megatron.sh
@@ -8,8 +8,11 @@ set -x
 # export WANDB_API_KEY=<your_key_here>
 # bash examples/terminal_bench/run_tbench_megatron.sh
 
+export WANDB_API_KEY="c59807d68ecbef2281925f42a59269b251a12b33"
+export WANDB_ENTITY="bespoke-labs"
+export PYTHONPATH="$PWD"
+
 DATA_DIR="$HOME/ez_apex_100"
-# DATA_DIR="$HOME/sandboxes-tasks"
 NUM_NODES=1
 NUM_GPUS=8
 LOGGER="wandb"  # change to "console" to print to stdout
@@ -29,8 +32,8 @@ MEGATRON_EP=4
 MEGATRON_ETP=2
 
 FLASH_ATTN=true
-NUM_INFERENCE_ENGINES=2
-INFERENCE_ENGINE_TP=4
+NUM_INFERENCE_ENGINES=8
+INFERENCE_ENGINE_TP=1
 
 # Torch profiler (optional)
 ENABLE_TORCH_PROFILER=false
@@ -41,16 +44,15 @@ export SKYRL_PYTHONPATH_EXPORT=1
 export CUDNN_PATH="$(uv run python -c 'import inspect, os, nvidia.cudnn as c; print(os.path.dirname(inspect.getfile(c)))')"
 export CPATH="$CUDNN_PATH/include:${CPATH:-}"
 export LD_LIBRARY_PATH="$CUDNN_PATH/lib:${LD_LIBRARY_PATH:-}"
-export VLLM_USE_FLASHINFER_SAMPLER=0
-export CUDA_LAUNCH_BLOCKING=1
-export CUDA_ENABLE_COREDUMP_ON_EXCEPTION=1
-export CUDA_COREDUMP_SHOW_PROGRESS=1
-export CUDA_COREDUMP_GENERATION_FLAGS='skip_nonrelocated_elf_images,skip_global_memory,skip_shared_memory,skip_local_memory,skip_constbank_memory'
-export CUDA_COREDUMP_FILE="/persistent_dir/cuda_coredump_%h.%p.%t"
-
+# export CUDA_LAUNCH_BLOCKING=1
+# export CUDA_ENABLE_COREDUMP_ON_EXCEPTION=1
+# export CUDA_COREDUMP_SHOW_PROGRESS=1
+# export CUDA_COREDUMP_GENERATION_FLAGS='skip_nonrelocated_elf_images,skip_global_memory,skip_shared_memory,skip_local_memory,skip_constbank_memory'
+# export CUDA_COREDUMP_FILE="/root/SkyRL/skyrl-train/cuda_coredump_%h.%p.%t"
+# export HYDRA_FULL_ERROR=1
 
 # data.train_data="['$DATA_DIR/train.parquet']" \
-uv run --isolated --extra $INFERENCE_BACKEND --extra sandboxes --extra mcore --with "sandboxes@./sandboxes" -m examples.terminal_bench.entrypoints.main_tbench \
+uv run --active --extra $INFERENCE_BACKEND --extra sandboxes --extra mcore --with "sandboxes@./sandboxes" -m examples.terminal_bench.entrypoints.main_tbench \
   data.train_data="['$DATA_DIR']" \
   hydra.searchpath=[file://$TBENCH_CONFIG_DIR] \
   +terminal_bench_config=terminal_bench \
@@ -92,7 +94,7 @@ uv run --isolated --extra $INFERENCE_BACKEND --extra sandboxes --extra mcore --w
   trainer.eval_before_train=false \
   trainer.eval_interval=-1 \
   trainer.update_epochs_per_batch=1 \
-  trainer.train_batch_size=4 \
+  trainer.train_batch_size=8 \
   trainer.policy_mini_batch_size=1 \
   trainer.micro_forward_batch_size_per_gpu=1 \
   trainer.micro_train_batch_size_per_gpu=1 \
@@ -107,17 +109,12 @@ uv run --isolated --extra $INFERENCE_BACKEND --extra sandboxes --extra mcore --w
   generator.async_engine=true \
   generator.batched=true \
   environment.env_class=terminal_bench \
-  generator.n_samples_per_prompt=4 \
-  generator.gpu_memory_utilization=0.5 \
+  generator.n_samples_per_prompt=8 \
+  generator.gpu_memory_utilization=0.7 \
   trainer.logger="$LOGGER" \
   trainer.project_name="terminal_bench" \
-  trainer.run_name="terminal_bench_megatron_tp${MEGATRON_TP}_pp${MEGATRON_PP}_cp${MEGATRON_CP}_${MODEL_NAME}" \
+  trainer.run_name="terminal_bench_megatron_H200_tp" \
   trainer.resume_mode=null \
   trainer.ckpt_path="$HOME/ckpts/ez_apex_ckpt" \
-  trainer.hf_save_interval=1000000 \
+  trainer.hf_save_interval=25 \
   $@
-
-
-# environment.env_class=gsm8k \
-
-# terminal_bench_config.max_episodes=16 \
