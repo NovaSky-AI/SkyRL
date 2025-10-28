1a545e27339a21107ff165010549b24d073a3c1b
SumanthRH <sumanthrh99@gmail.com>
2025-10-28 20:25:52 +0000

    x
    Signed-off-by: SumanthRH <sumanthrh99@gmail.com>

--- skyrl-agent/skyrl_agent/integrations/skyrl_train/trainer.py
+++ skyrl-agent/skyrl_agent/integrations/skyrl_train/trainer.py
@@ -274,8 +274,8 @@ class SkyRLAgentPPOTrainer(RayPPOTrainer):
 
         # main training loop
         pbar = tqdm(total=self.total_training_steps, initial=self.global_step, desc="Training Batches Processed")
-        self.global_step += 1  # start training at global_step 1
         start_epoch = self.global_step // len(self.train_dataloader)
+        self.global_step += 1  # start training at global_step 1
         for epoch in range(start_epoch, self.cfg.trainer.epochs):
             for iter, rand_prompts in enumerate(self.train_dataloader):
                 with Timer("step", self.all_timings):
