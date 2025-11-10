c918cdd96ff77834f471a67a01b15ec31ffd61aa
Charlie Ruan <charlieruan@berkeley.edu>
2025-11-10 22:02:52 +0000

    update script

--- skyrl-train/examples/fully_async/async_run_gsm8k.sh
+++ skyrl-train/examples/fully_async/async_run_gsm8k.sh
@@ -18,14 +18,21 @@ set -x
 : "${DATA_DIR:="$HOME/data/gsm8k"}"
 : "${NUM_INFERENCE_GPUS:=2}"
 : "${NUM_POLICY_GPUS:=2}"
-: "${LOGGER:=console}" # change to "console" to print to stdout / or use wandb
+: "${LOGGER:=wandb}" # change to "console" to print to stdout / or use wandb
 
 : "${INFERENCE_BACKEND:=vllm}"
 # : "${INFERENCE_BACKEND:=sglang}"
 
+# Fully async specific configuration knobs:
+: "${MINI_BATCH_SIZE:=256}"
+: "${MAX_STALENESS_STEPS:=4}"
+: "${NUM_PARALLEL_GENERATION_WORKERS:=$(( MINI_BATCH_SIZE * (MAX_STALENESS_STEPS + 1) ))}"
+
 uv run --isolated --extra $INFERENCE_BACKEND -m examples.fully_async.main_async \
   data.train_data="['$DATA_DIR/train.parquet']" \
   data.val_data="['$DATA_DIR/validation.parquet']" \
+  trainer.fully_async.max_staleness_steps=${MAX_STALENESS_STEPS} \
+  trainer.fully_async.num_parallel_generation_workers=${NUM_PARALLEL_GENERATION_WORKERS} \
   trainer.algorithm.advantage_estimator="grpo" \
   trainer.policy.model.path="Qwen/Qwen2.5-1.5B-Instruct" \
   trainer.placement.colocate_all=false \
@@ -38,10 +45,10 @@ uv run --isolated --extra $INFERENCE_BACKEND -m examples.fully_async.main_async 
   trainer.epochs=20 \
   trainer.eval_batch_size=1024 \
   trainer.eval_before_train=false \
-  trainer.eval_interval=10 \
+  trainer.eval_interval=4 \
   trainer.update_epochs_per_batch=1 \
-  trainer.train_batch_size=256 \
-  trainer.policy_mini_batch_size=256 \
+  trainer.train_batch_size=${MINI_BATCH_SIZE} \
+  trainer.policy_mini_batch_size=${MINI_BATCH_SIZE} \
   trainer.micro_forward_batch_size_per_gpu=8 \
   trainer.micro_train_batch_size_per_gpu=8 \
   trainer.ckpt_interval=10 \
@@ -60,7 +67,7 @@ uv run --isolated --extra $INFERENCE_BACKEND -m examples.fully_async.main_async 
   trainer.logger="$LOGGER" \
   trainer.project_name="gsm8k-async" \
   trainer.run_name="gsm8k-test-fully_async-4xl4" \
-  trainer.resume_mode=null \
+  trainer.resume_mode=latest \
   trainer.ckpt_path="$HOME/ckpts/gsm8k_1.5B_ckpt" \
   generator.chat_template.source=name \
   generator.chat_template.name_or_path="qwen2_5_with_generation_tag_simplified" \
