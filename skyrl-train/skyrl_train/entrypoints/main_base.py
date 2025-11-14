07f5beab0325617473aa9f1caaa56c7267096678
Dev Patel <dev.patel@berkeley.edu>
2025-11-14 05:01:04 +0000

    linter

--- skyrl-train/skyrl_train/entrypoints/main_base.py
+++ skyrl-train/skyrl_train/entrypoints/main_base.py
@@ -66,14 +66,14 @@ def create_ray_wrapped_inference_engines_from_config(cfg: DictConfig, colocate_p
         engine_kwargs["max_lora_rank"] = cfg.trainer.policy.model.lora.rank
         engine_kwargs["sleep_level"] = 1
         engine_kwargs["max_loras"] = 1
-       
+
         tp_size = cfg.generator.inference_engine_tensor_parallel_size
         if cfg.generator.backend == "vllm" and tp_size > 1:
             # try fully-sharded-loras (adjust heuristics)
             if lora_rank >= 64 or tp_size >= 4:
                 engine_kwargs["fully_sharded_loras"] = True
-        
-        #TODO(devpatel): Bandaid solution, replace this once we have a better solution for LoRA performance degradation on the vLLM side
+
+        # TODO(devpatel): Bandaid solution, replace this once we have a better solution for LoRA performance degradation on the vLLM side
         if cfg.generator.enforce_eager and cfg.generator.backend == "vllm":
             logger.warning(
                 "LoRA is enabled but generator.enforce_eager=true. "
