0041f389d4c0d5e8027dcfedbe640385163b3e12
SumanthRH <sumanthrh99@gmail.com>
2025-12-17 16:58:36 +0000

    x
    Signed-off-by: SumanthRH <sumanthrh99@gmail.com>

--- skyrl-train/skyrl_train/workers/fsdp/fsdp_worker.py
+++ skyrl-train/skyrl_train/workers/fsdp/fsdp_worker.py
@@ -251,7 +251,7 @@ class FSDPPolicyWorkerBase(PolicyWorkerBase):
                         if self.cfg.generator.weight_sync_backend == "nccl" and self.cfg.generator.backend == "vllm":
                             self._model_update_group.broadcast(tensor.data, src=0, stream=torch.cuda.current_stream())
                         else:
-                            torch.distributed(tensor.data, 0, group=self._model_update_group)
+                            torch.distributed.broadcast(tensor.data, 0, group=self._model_update_group)
 
                 await asyncio.to_thread(broadcast_tensor, tensor)
                 if torch.distributed.get_rank() == 0:
