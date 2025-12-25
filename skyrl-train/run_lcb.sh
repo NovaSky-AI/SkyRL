08908173ffb39c1e1ad6faf0f6c6dc42da986c87
Harryllh <liheng@berkeley.edu>
2025-12-24 18:37:32 -0800

    updated bash script

--- skyrl-train/run_lcb.sh
+++ skyrl-train/run_lcb.sh
@@ -4,23 +4,42 @@ set -x
 # export WANDB_API_KEY=<your_key_here>
 # bash examples/livecodebench/run_lcb.sh
 
+export WANDB_API_KEY="6a25ce5b41815c557d6fe8aecb8bac2dd6b1bea0"
+
+
+NUM_NODES=1
+NUM_GPUS=1
+LOGGER="wandb"
+
+MODEL_NAME="Qwen/Qwen3-8B"
+
+
+FLASH_ATTN=true
+NUM_INFERENCE_ENGINES=1
+INFERENCE_ENGINE_TP=1
+
 DATA_DIR="$HOME/data/lcb"
-train_data="['${DATA_DIR}/deepcoder_train.json']"
-val_data="['${DATA_DIR}/test_livecodebench.json']"
+train_data="['${DATA_DIR}/deepcoder_train_short.json']"
+val_data="['${DATA_DIR}/test_livecodebench_short.json']"
 
 # NOTE (sumanthrh): micro_train_batch_size and micro_forward_batch_size can be tuned
 uv run --isolated --frozen --extra vllm -m skyrl_train.entrypoints.main_base \
   trainer.algorithm.advantage_estimator="grpo" \
   data.train_data=$train_data \
-  data.val_data=$val_data \
-  trainer.policy.model.path="Qwen/Qwen2.5-3B-Instruct" \
+  data.val_data=$train_data \
+  trainer.policy.model.path=$MODEL_NAME \
   trainer.placement.colocate_all=true \
   trainer.strategy=fsdp2 \
   trainer.policy.optimizer_config.max_grad_norm=0.5 \
-  trainer.placement.policy_num_gpus_per_node=8 \
-  trainer.placement.ref_num_gpus_per_node=8 \
-  generator.num_inference_engines=2 \
-  generator.inference_engine_tensor_parallel_size=4 \
+  trainer.placement.policy_num_nodes=$NUM_NODES \
+  trainer.placement.ref_num_nodes=$NUM_NODES \
+  trainer.placement.policy_num_gpus_per_node=$NUM_GPUS \
+  trainer.placement.ref_num_gpus_per_node=$NUM_GPUS \
+  generator.num_inference_engines=$NUM_INFERENCE_ENGINES \
+  generator.inference_engine_tensor_parallel_size=$INFERENCE_ENGINE_TP \
+  generator.enable_http_endpoint=true \
+  generator.http_endpoint_host="127.0.0.1" \
+  generator.http_endpoint_port=8000 \
   trainer.policy_mini_batch_size=4 \
   trainer.train_batch_size=16 \
   trainer.micro_forward_batch_size_per_gpu=16 \
@@ -32,13 +51,14 @@ uv run --isolated --frozen --extra vllm -m skyrl_train.entrypoints.main_base \
   trainer.algorithm.use_kl_loss=true \
   trainer.algorithm.kl_loss_coef=0.001 \
   trainer.ckpt_interval=100000 \
+  trainer.flash_attn=false \
   generator.backend=vllm \
   generator.run_engines_locally=true \
   generator.weight_sync_backend=nccl \
   generator.async_engine=true \
   generator.batched=false \
   environment.env_class=lcb \
-  generator.n_samples_per_prompt=5 \
+  generator.n_samples_per_prompt=8 \
   generator.gpu_memory_utilization=0.7 \
   generator.sampling_params.temperature=0.6 \
   generator.sampling_params.top_p=0.95 \
