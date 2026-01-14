31c30cc4f04c8a0c55b6075b11acd404550bb3bf
Eric Tang <erictang000@gmail.com>
2026-01-14 00:16:09 +0000

    x

--- skyrl-train/examples/async/async_trainer.py
+++ skyrl-train/examples/async/async_trainer.py
@@ -44,7 +44,7 @@ class AsyncRayPPOTrainer(RayPPOTrainer):
                 self.tracker.log(eval_metrics, step=self.global_step)
 
         # main training loop
-        pbar = tqdm(total=self.total_training_batches, initial=self.global_step, desc="Training Step Progress")
+        pbar = tqdm(total=self.total_training_batches, initial=self.global_step, desc="Training Batch Progress")
         start_epoch = self.global_step // len(self.train_dataloader)
         # Start from step 1
         self.global_step += 1
