f1fb36cef3c163a26dcbfe4401f80f730ea0bd4e
Philipp Moritz <pcmoritz@gmail.com>
2026-01-18 16:50:56 -0800

    update schedule

--- skyrl-tx/tx/ffi/ragged_dot_ffi.cu
+++ skyrl-tx/tx/ffi/ragged_dot_ffi.cu
@@ -63,8 +63,9 @@ using OperatorClass = cutlass::arch::OpClassTensorOp;
 using TileShape = cute::Shape<cute::_128, cute::_128, cute::_64>;
 // Use 2x1x1 cluster on H100 for better L2 cache utilization
 using ClusterShape = cute::Shape<cute::_2, cute::_1, cute::_1>;
-using KernelSchedule = cutlass::gemm::KernelPtrArrayTmaWarpSpecializedPingpong;
-using EpilogueSchedule = cutlass::epilogue::PtrArrayTmaWarpSpecializedPingpong;
+// Cooperative schedule for better work distribution across thread blocks
+using KernelSchedule = cutlass::gemm::KernelPtrArrayTmaWarpSpecializedCooperative;
+using EpilogueSchedule = cutlass::epilogue::PtrArrayTmaWarpSpecializedCooperative;
 using ProblemShape = cutlass::gemm::GroupProblemShape<
     cute::Shape<int32_t, int32_t, int32_t>>;
 
