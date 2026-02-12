2480bce33902e98ed10979b8391518c69fd6d8d1
Eric Tang <erictang000@gmail.com>
2026-02-12 21:48:03 +0000

    use structure of repo roughly

--- skyrl/ci/gpu_ci_run_skyrl_train.sh
+++ skyrl/ci/gpu_ci_run_skyrl_train.sh
@@ -5,5 +5,5 @@ export CI=true
 # Prepare datasets used in tests.
 uv run examples/gsm8k/gsm8k_dataset.py --output_dir $HOME/data/gsm8k
 # Run all non-megatron tests
-uv run --directory . --isolated --extra dev --extra fsdp pytest -s tests/skyrl_train_tests/gpu/gpu_ci -m "not megatron"
+uv run --directory . --isolated --extra dev --extra fsdp pytest -s tests/backends/skyrl_train/gpu/gpu_ci -m "not megatron"
 
