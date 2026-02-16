b9d18e02f28da0e5cf333f84a67c6124688ab785
Charlie Ruan <charlieruan@berkeley.edu>
2026-02-16 06:35:25 +0000

    more fixes

--- examples/train_integrations/openenv/entrypoints/main_openenv.py
+++ examples/train_integrations/openenv/entrypoints/main_openenv.py
@@ -4,14 +4,15 @@ from skyrl.train.entrypoints.main_base import BasePPOExp, config_dir, validate_c
 from skyrl.train.utils import initialize_ray
 import ray
 from skyrl_gym.envs import register
+from ..env import OpenEnv
 
 
 @ray.remote(num_cpus=1)
 def skyrl_entrypoint(cfg: DictConfig):
-    # Register the multiply environment inside the entrypoint task (no need to modify the skyrl-gym package).
+    # Register the openenv environment inside the entrypoint task (no need to modify the skyrl-gym package).
     register(
         id="openenv",
-        entry_point="integrations.openenv.env:OpenEnv",
+        entry_point=OpenEnv,
     )
     exp = BasePPOExp(cfg)
     exp.run()
