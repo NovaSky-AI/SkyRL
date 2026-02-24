d1829f516512812cfa41637944d6af78a63b9533
SumanthRH <sumanthrh99@gmail.com>
2026-02-24 00:29:52 +0000

    x
    Signed-off-by: SumanthRH <sumanthrh99@gmail.com>

--- ci/gpu_e2e_test_run.sh
+++ ci/gpu_e2e_test_run.sh
@@ -2,6 +2,4 @@
 set -euo pipefail
 
 uv run examples/train/gsm8k/gsm8k_dataset.py --output_dir $HOME/data/gsm8k
-# override run name for bookkeeping
 bash tests/train/gpu_e2e_test/gsm8k_colocate.sh
-
