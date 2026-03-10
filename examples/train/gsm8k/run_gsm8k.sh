2d39dc24b2e08dc5f553f9ffecb642c0cc1cd76e
Eric Tang <erictang000@gmail.com>
2026-03-10 20:42:28 +0000

    x

--- examples/train/gsm8k/run_gsm8k.sh
+++ examples/train/gsm8k/run_gsm8k.sh
@@ -21,13 +21,13 @@ uv run --isolated --extra fsdp -m skyrl.train.entrypoints.main_base \
   data.val_data="['$DATA_DIR/validation.parquet']" \
   trainer.algorithm.advantage_estimator="grpo" \
   trainer.policy.model.path="Qwen/Qwen2.5-1.5B-Instruct" \
-  trainer.placement.colocate_all=true \
+  trainer.placement.colocate_all=false \
   trainer.strategy=fsdp2 \
-  trainer.placement.policy_num_gpus_per_node=$NUM_GPUS \
-  trainer.placement.critic_num_gpus_per_node=$NUM_GPUS \
-  trainer.placement.ref_num_gpus_per_node=$NUM_GPUS \
-  generator.inference_engine.num_engines=$NUM_GPUS \
-  generator.inference_engine.tensor_parallel_size=1 \
+  trainer.placement.policy_num_gpus_per_node=2 \
+  trainer.placement.critic_num_gpus_per_node=2 \
+  trainer.placement.ref_num_gpus_per_node=2 \
+  generator.inference_engine.num_engines=1 \
+  generator.inference_engine.tensor_parallel_size=2 \
   trainer.epochs=20 \
   trainer.eval_batch_size=1024 \
   trainer.eval_before_train=true \
@@ -43,6 +43,7 @@ uv run --isolated --extra fsdp -m skyrl.train.entrypoints.main_base \
   trainer.policy.optimizer_config.lr=1.0e-6 \
   trainer.algorithm.use_kl_loss=true \
   generator.inference_engine.backend=$INFERENCE_BACKEND \
+  generator.inference_engine.distributed_executor_backend=mp \
   generator.inference_engine.run_engines_locally=true \
   generator.inference_engine.weight_sync_backend=nccl \
   generator.inference_engine.async_engine=true \
