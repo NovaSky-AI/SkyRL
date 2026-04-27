e4fe94d7d3ebbd42834c1c4ed9f459dd2e21d56f
Eric Tang <erictang000@gmail.com>
2026-04-27 20:33:11 +0000

    x

--- ci/gpu_ci_run_skyrl_train_megatron_models.sh
+++ ci/gpu_ci_run_skyrl_train_megatron_models.sh
@@ -4,6 +4,6 @@ set -xeuo pipefail
 export CI=true
 # Prepare datasets used in tests.
 uv run examples/train/gsm8k/gsm8k_dataset.py --output_dir $HOME/data/gsm8k
-# Run all megatron tests
+# Run megatron model tests
 uv run --directory . --isolated --extra dev --extra megatron pytest -s tests/backends/skyrl_train/gpu/gpu_ci -m "megatron_models"
 
