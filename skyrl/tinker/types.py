f36c9ef844894fdc4efd9b902df081fed616df55
Charles Tang <j316chuck@users.noreply.github.com>
2026-08-05 22:02:41 -0700

    Apply suggestion from @j316chuck

--- skyrl/tinker/types.py
+++ skyrl/tinker/types.py
@@ -208,7 +208,7 @@ class SaveWeightsOutput(BaseModel):
 class LoadWeightsInput(BaseModel):
     source_model_id: str
     checkpoint_id: str
-    optimizer: bool
+    optimizer: bool = True
 
 
 class LoadWeightsOutput(BaseModel):
