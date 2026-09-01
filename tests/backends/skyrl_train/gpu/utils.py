858086a7cd01a190b9b219e3f6b30aebe05803bb
SumanthRH <sumanthrh@anyscale.com>
2026-09-01 23:47:41 +0000

    x
    Signed-off-by: SumanthRH <sumanthrh@anyscale.com>

--- tests/backends/skyrl_train/gpu/utils.py
+++ tests/backends/skyrl_train/gpu/utils.py
@@ -391,7 +391,7 @@ def ray_init_for_tests():
         env_vars["PYTHONPATH"] = os.environ.get("PYTHONPATH")
     env_vars["CUDA_DEVICE_MAX_CONNECTIONS"] = "1"
     env_vars["NVTE_FUSED_ATTN"] = "0"
-    env_vars["LD_LIBRARY_PATH"] = os.environ.get("LD_LIBRARY_PATH")
+    env_vars["LD_LIBRARY_PATH"] = os.environ.get("LD_LIBRARY_PATH", "")
     ray.init(runtime_env={"env_vars": env_vars})
 
 
